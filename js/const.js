81e910a13da1d30f1baa50a7f2459c1575e66ee3
9Novem9 <104360816+9Novem9@users.noreply.github.com>
2024-05-02 13:52:59 +0900

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,1 +1,6 @@
-WSURL = 'ws://localhost:5000';
+// WSURL = 'wss://carasorasp.omatz.cf';
+// WSURL = 'wss://cataso2.omatz.cf';
+// WSURL = 'wss://cataso-omatz.herokuapp.com.';
+// WSURL = 'ws://localhost:5000';
+
+WSURL = 'wss://nageoooo.onrender.com';
